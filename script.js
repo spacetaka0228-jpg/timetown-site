167413cfc658a37953995220f55b6deb16ae8251
naoitakahiro <spacetaka0228@gmail.com>
2026-03-11 23:25:32 +0900

    メニューボタン実装

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ const button = document.getElementById("menuButton");
 const drawer = document.getElementById("drawerNav");
 
 button.addEventListener("click", () => {
-    button.classList.toggle("is-open");
     drawer.classList.toggle("is-open");
+
+    if (button.classList.contains("is-open")) {
+        button.classList.remove("is-open");
+        button.classList.add("is-close");
+    } else {
+        button.classList.remove("is-close");
+        button.classList.add("is-open");
+    }
 });
