d0f2d5d1a003b11db59715e1e58bf8631fd73151
fck naoi <spacetaka0228@gmail.com>
2026-03-11 17:21:15 +0900

    fck　変更：メニューボタンのアニメーション

--- script.js
+++ script.js
@@ -4,5 +4,6 @@ const button = document.getElementById("menuButton");
 const drawer = document.getElementById("drawerNav");
 
 button.addEventListener("click", () => {
+    button.classList.toggle("is-open");
     drawer.classList.toggle("is-open");
 });
